ccdf946c8f3400c815ad35653ca5f236b3ada2e4
ascerbishka <ascer655@gmail.com>
2025-12-18 17:34:59 +0300

    feat: Create initial script

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ author_name = "Aziov Ascerbi" #
 print("Hello, World!")
 print(f"I am a future engineer, and my name is {author_name}.")
 print("This project is now on GitHub!")
+
+name = "Artorias"
+char_clas = "The Traveler of the Abyss"
+level = 300
+print ('Boss DS1:',name)
+print ('Class:',char_clas)
+print ('Boss lvl:',level)
